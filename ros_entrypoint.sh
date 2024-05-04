f81e4ff186ba8dee5646981cd7d40faee4bdc145
Sirio Jansen-Sanchez <siriojansen@hotmail.com>
2024-05-04 16:03:14 -0400

    CAN Docker Debugging

--- ros_entrypoint.sh
+++ ros_entrypoint.sh
@@ -2,8 +2,11 @@
 set -e
 
 export ROS_DOMAIN_ID=69
+# Source ROS
 source /opt/ros/humble/setup.bash
+# Source Compiled Workspace
 source /app/install/setup.bash
+# Source CYPHAL Environment
 source /app/scripts/cyphal_env_source.sh
 
 exec "$@"
