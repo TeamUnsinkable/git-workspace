f3b0cc07632b212cf99ee19a00e2230f6eb2db56
markt006 <150852139+markt006@users.noreply.github.com>
2024-08-09 02:28:16 -0700

    Fixed node logic, added sonar python name definition

--- src/br_ping360_py/br_ping360_py/ping1D_node.py
+++ src/br_ping360_py/br_ping360_py/ping1D_node.py
@@ -1,10 +1,13 @@
 import rclpy
 from rclpy.node import Node
 from rclpy.timer import Timer
-from br_ping360_py.br_ping360_py.ping1D_sonar import SonarReader
+from std_msgs.msg import Float64
+from br_ping360_py.ping1D_sonar import SonarReader
+from br_ping360_msgs.msg import Ping1D
 from rclpy.service import SrvTypeRequest, SrvTypeResponse
 import numpy as np
 import threading
+
 
 class SonarNode(Node):
     def __init__(self):
@@ -15,37 +18,43 @@ class SonarNode(Node):
         self._load_params()
 
         # Configure sonar driver
-        self.sonar.configure_params()
-        distance, confidence = self.sonar.scan()    
+        self.sonar.configure_params()    
+        self.lock = threading.Lock()
 
     def _load_params(self):
         # Connection Parameters
         self.declare_parameter("port", "/dev/ttyUSB1")
         self.declare_parameter("baud", 115200)
 
-        # Declare Parameters
-        #self.declare_parameter("gain_setting", 0)
-        #self.declare_parameter('angle_start', 0)
-        #self.declare_parameter('angle_stop', 400)
-        #self.declare_parameter('transmit_frequency', 750)
-        #self.declare_parameter('number_of_samples', 1200)
+        # Operational Parameters
+        self.declare_parameter("gain_setting", 0)
+        self.declare_parameter('transmit_frequency', 750)
         self.declare_parameter('speed_of_sound', 1500)
-        #self.declare_parameter('range', 10)
-        #self.declare_parameter('delay', 4)
-
+        self.declare_parameter('range', 10)
+        self.declare_parameter('rate', 10)
+        
         # Load Parameters
         self.port     = self.get_parameter("port")
         self.baud      = self.get_parameter("baud")
 
-        #self.gain_setting       = self.get_parameter("gain_setting").get_parameter_value().integer_value
-        #self.angle_start        = self.get_parameter("angle_start").get_parameter_value().integer_value
-        #self.angle_stop         = self.get_parameter("angle_stop").get_parameter_value().integer_value
-        #self.transmit_frequency = self.get_parameter('transmit_frequency').get_parameter_value().integer_value
-        #self.number_of_samples  = self.get_parameter('number_of_samples').get_parameter_value().integer_value
+        self.gain_setting       = self.get_parameter("gain_setting").get_parameter_value().integer_value
+        self.transmit_frequency = self.get_parameter('transmit_frequency').get_parameter_value().integer_value
         self.speed_of_sound     = self.get_parameter('speed_of_sound').get_parameter_value().integer_value
-        #self.range              = self.get_parameter('range').get_parameter_value().integer_value
-        #self.delay              = self.get_parameter('delay').get_parameter_value().integer_value
+        self.range              = self.get_parameter('range').get_parameter_value().integer_value
+        self.rate               = self.get_parameter('rate').get_parameter_value().integer_value
+        # Create publishers
+        # TODO: Fix Publisher initialization
+        self.pub_distance = self.create_publisher(Ping1D, "sonar/one_dim", 5)
+        self.timer = self.create_timer(1/rate, self.timer_callback)
         
+
+    def timer_callback(self) -> None:
+        value = Ping1D()
+        self.lock.acquire(True)
+        value.distance, value.confidence = self.sonar.scan()
+        self.pub_distance.publish(value)
+        self.lock.release()
+
     
 def main():
     rclpy.init()
--- src/br_ping360_py/br_ping360_py/ping1D_sonar.py
+++ src/br_ping360_py/br_ping360_py/ping1D_sonar.py
@@ -3,7 +3,7 @@ from brping import PingDevice, Ping1D, PingMessage
 import numpy as np
 
 def SonarReader():
-    def __init__(self, port="/dev/ttyUSB1", baud=115200) -> bool:
+    def __init__(self, port="/dev/ttyUSB0", baud=115200) -> bool:
         self.sonar = Ping1D()
         self.sonar.connect_serial(port, baud)
         return self.sonar.initialize()
@@ -15,43 +15,6 @@ def SonarReader():
         return self.sonar.get_distance_simple()
     
 
-    ############################
-    ## Parameter Calculations ##
-    ############################
-
-    ##def _calculate_sample_period(self):
-        ##return  int((2 * self.range) / (self.number_of_samples * self.speed_of_sound * 25*(10**-9)))
+if __name__ == "__main__":
+    sonar = SonarReader()
     
-    ## def _calculate_transmit_duration(self):
-        """
-        Per firmware engineer:
-            1. Starting point is TxPulse in usec = ((one-way range in metres) * 8000) / (Velocity of sound in metres per second)
-            2. Then check that TxPulse is wide enough for currently selected sample interval in usec, i.e.,
-                if TxPulse < (2.5 * sample interval) then TxPulse = (2.5 * sample interval)
-            3. Perform limit checking
-        """
-        ##inital_transmit = round((self.range * 8000) / self.speed_of_sound)
-        ##duration = max((2.5 * self.sample_period) / 1000, inital_transmit)
-        ##onar_min_duration = 5
-        ##transmit_max = min(self.sample_period*(64*10**6), 500)
-        ##transmit_duration = max(sonar_min_duration, min(duration, transmit_max))
-        ##return transmit_duration
-
- 
-    ##def _validate_parameters(self):
-        #Gain Values
-      ##  if self.gain_setting not in [0,1,2]:
-       ##     raise ValueError("Gain Values not valid")
-        #Angle scan values
-       ## if self.angle_start > self.angle_end:
-         ##   raise ValueError("Scan angle start can not be greater than scan angle end")
-       ## elif self.angle_start > 400 or self.angle_start < 0:
-          ##  raise ValueError("Scan angle start must be between 0 and 400 inclusive")
-       ## elif self.angle_end > 400 or self.angle_end < 0:
-         ##   raise ValueError("Scan andle end can must be between 400 and 0 inclusive")
-        #Transmit Frequency
-        ##if self.transmit_frequency < 650 or self.transmit_frequency > 850:
-          ##  raise ValueError("Transmit frequency must be between 650 and 850")
-       ## if self.number_of_samples < 200 or self.number_of_samples > 1200:
-         ##   raise ValueError("Number of samples should be between 200 and 1200 inclusive")
-        
